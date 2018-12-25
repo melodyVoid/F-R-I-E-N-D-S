850fb133fcb2be567563b14d4c8aa9a116d80070
melodyVoid <botaoxyz@gmail.com>
2018-12-25 18:20:18 +0800

    episode1 season1

---
+++ publish.sh
@@ -0,0 +1,9 @@
+rm -rf ./_book
+gitbook build
+git checkout gh-pages
+git pull origin gh-pages
+cp -rf ./_book/* ./
+git add .
+git commit -m 'regenerated book automatically'
+git push origin gh-pages
+git checkout master
